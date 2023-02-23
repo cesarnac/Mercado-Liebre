a12a0f1c16e9a53bb9f1060c0bd2b4ca56bb4074
cesarnac@gmail.com <cesarnac@gmail.com>
2023-02-22 19:21:00 -0500

    actualizando

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/home.html"))
 })
 
+app.get("/register",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/register.html"))
+})
+
+app.get("/login",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/login.html"))
+})
+
 
 const port = process.env.PORT || 3000;
 
